03c8db554eedbb7c1fe01a04155aa8a9dd0108c6
omarbabou <obabou50@gmail.com>
2024-09-22 15:23:50 +0200

    Implementing appBar icon button

---
+++ lib/widgets/expenses_list/expense_item.dart
@@ -0,0 +1,42 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpenseItem extends StatelessWidget {
+  const ExpenseItem(this.expense, {super.key});
+
+  final Expense expense;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 16,
+          horizontal: 20,
+        ),
+        child: Column(
+          children: [
+            Text(
+              expense.title,
+            ),
+            const SizedBox(height: 4),
+            Row(
+              children: [
+                Text(
+                  '\$${expense.amount.toStringAsFixed(2)}'), // toStringAsFixed convert 12.3433 => 12.34
+                const Spacer(),
+                 Row(
+                 children: [
+                  Icon(categoryIcons[expense.category]),
+                    const SizedBox(width: 8),
+                    Text(expense.formattedDate),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
