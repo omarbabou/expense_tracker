a7d42ddddea6f5fb7768679878379ce69e4cac61
omarbabou <obabou50@gmail.com>
2024-09-23 18:03:00 +0200

    Implementing charts widgets functionalities and styles

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/material.dart';
+
+import 'package:expense_tracker/widgets/chart/chart.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:expense_tracker/widgets/new_expense.dart';
-import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -88,7 +90,7 @@ class _ExpensesState extends State<Expenses> {
       ),
       body: Column(
         children: [
-          const Text('The chart'),
+          Chart(expenses: _registeredExpenses),
           Expanded(
             child: mainContent,
           ),
